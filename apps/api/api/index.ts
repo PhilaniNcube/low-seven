2a35b2a2d764b14b7d57eb31a3f26e6602c6e134
PhilaniNcube <ncbphi001@gmail.com>
2026-01-26 16:20:41 +0200

    Remove build step - let Vercel handle TypeScript compilation natively

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -1,7 +1,4 @@
 import { handle } from '@hono/node-server/vercel'
 import app from '../src/index'
 
-// Vercel automatically provides environment variables
-// No need to call dotenv.config() in serverless environment
-
 export default handle(app)
